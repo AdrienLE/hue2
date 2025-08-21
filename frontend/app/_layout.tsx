56f8467b981c76b97e4fba04eacc30c3f45fcc2a
Adrien Ecoffet <adrien.ecoffet@gmail.com>
2025-08-20 21:43:05 -0700

    fix favicon

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -264,8 +264,10 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Head>
         <title>Hue - Habit Tracker</title>
-        {/* Static favicon for production web */}
-        <link rel="icon" href="/favicon.ico" />
+        {/* High-res favicons for production web */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <View
         style={{
